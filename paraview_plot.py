c7b700c1ca2b05a9371053466440951ec3710e03
Cameron Devine <camerondtrain@gmail.com>
2018-02-14 14:11:03 -0800

    added support for creating a video to the plot script

--- paraview_plot.py
+++ paraview_plot.py
@@ -9,4 +9,15 @@ PlotOverLine(reader)
 
 Show()
 Render()
-AnimateReader(reader)
+
+outfile = None
+try:
+	outfile = sys.argv[2]
+except:
+	pass
+AnimateReader(reader, filename = '{}.avi'.format(outfile))
+
+if outfile is not None:
+	import subprocess, os
+	subprocess.call('mencoder -speed 45 -o {0}.mp4 -ovc lavc {0}.avi'.format(outfile).split(' '))
+	os.remove('{}.avi'.format(outfile))
