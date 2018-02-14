82d69f63c53ffe201218c8a10bc1033d18a78a79
Cameron Devine <camerondtrain@gmail.com>
2018-02-14 12:48:18 -0800

    changed the plot script to only plot one variable at once.

--- paraview_plot.py
+++ paraview_plot.py
@@ -1,18 +1,12 @@
+import sys
 from paraview.simple import *
 
-files = [
-	'spin_transport_soln/rho_1.pvd',
-	'spin_transport_soln/rho_2.pvd',
-	'spin_transport_soln/rho_3.pvd']
-
-readers = [PVDReader(FileName = f) for f in files]
+reader = PVDReader(FileName = sys.argv[1])
 
 UpdatePipeline()
 
-lines = [PlotOverLine(reader) for reader in readers]
+PlotOverLine(reader)
 
-[Show(line) for line in lines]
-
+Show()
 Render()
-
-input('press enter to exit')
+AnimateReader(reader)
