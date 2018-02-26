8b3a9037f0473fa87711d026dbef31e174d67f81
Cameron Devine <camerondtrain@gmail.com>
2018-02-26 12:51:14 -0800

    updated the test script

--- test.py
+++ test.py
@@ -1,26 +1,26 @@
 import unittest
 from spin_transport_01 import *
-import numpy as np	
-	
+import numpy as np
+
 class TestSSSolution(unittest.TestCase):
 
 	def sim_test(self, solution, tol, **kwargs):
+		kwargs['quiet'] = True
 		data = simulate(**kwargs)
-		result = True
 		for key in solution:
-			result = result and np.all(np.abs(solution[key](data['x']) - data[key]) < tol)
-		if not result:
-			print('plot error')
-		self.assertTrue(result)
+			self.assertTrue(
+				np.all(np.abs(
+					solution[key](data['x']) - data[key][-1,:]) < tol),
+				'{} failed.'.format(key))
 
 	def test_SS(self):
 		self.sim_test(
 			{
-				'rho1': lambda x: 0.1,
-				'rho2': lambda x: np.tanh(x),
-				'rho3': lambda x: np.tanh(x)
+				'rho1': lambda x: np.tanh(((1 + γb * δb) / (γb * (1 + δb))) * (μp * Bd / (kB * temp))),
+				'rho2': lambda x: np.tanh(μe * (Grad * x) / (kB * temp)),
+				'rho3': lambda x: np.tanh(μp * (Grad * x) / (kB * temp))
 			},
-			0.01)
+			0.005)
 
 if __name__ == '__main__':
 	unittest.main()
