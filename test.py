8ad071248e3f2986cbbc3e38bb03ac865494403e
Cameron Devine <camerondtrain@gmail.com>
2018-04-02 13:43:56 -0700

    added working tests to the unittest file.

--- test.py
+++ test.py
@@ -2,6 +2,49 @@ import unittest
 from spin_transport_01 import *
 import numpy as np
 
+class TestBaseSys(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(self):
+		self.ic = [0.1, 0.2, 0.3]
+		self.data = simulate(
+			Seperation = False,
+			Diffusion = False,
+			Bloch = False,
+			Pulse = False,
+			ic = [str(_) for _ in self.ic],
+			quiet = True)
+
+	def test_rho1(self):
+		self.assertEqual(self.data['rho1'][-1,:].tolist(), [self.ic[0]] * self.data['rho1'].shape[1])
+
+	def test_rho2(self):
+		self.assertEqual(self.data['rho2'][-1,:].tolist(), [self.ic[1]] * self.data['rho2'].shape[1])
+
+	def test_rho3(self):
+		self.assertEqual(self.data['rho3'][-1,:].tolist(), [self.ic[2]] * self.data['rho3'].shape[1])
+
+class TestWithoutBloch(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(self):
+		self.ic = [0, 0.1, -0.2]
+		self.data = simulate(
+			Bloch = False,
+			Pulse = False,
+			ic = [str(_) for _ in self.ic],
+			quiet = True)
+
+	def test_rho1(self):
+		self.assertEqual(self.data['rho1'][-1,:].tolist(), [self.ic[0]] * self.data['rho1'].shape[1])
+
+	def test_rho2(self):
+		self.assertEqual(self.data['rho2'][-1,:].tolist(), [self.ic[1]] * self.data['rho2'].shape[1])
+
+	def test_rho3(self):
+		self.assertEqual(self.data['rho3'][-1,:].tolist(), [self.ic[2]] * self.data['rho3'].shape[1])
+
+'''
 class TestSSSolution(unittest.TestCase):
 
 	def sim_test(self, solution, tol, **kwargs):
@@ -21,6 +64,7 @@ class TestSSSolution(unittest.TestCase):
 				'rho3': lambda x: np.tanh(μp * (Grad * x) / (kB * temp))
 			},
 			0.005)
+'''
 
 if __name__ == '__main__':
 	unittest.main()
