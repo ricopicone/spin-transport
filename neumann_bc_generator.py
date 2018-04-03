d6ba3072ab31f24c725932af651e500f721c80bb
Rico Picone <ricopicone@gmail.com>
2018-04-03 14:58:15 -0700

    fixed scaling with Bd

--- neumann_bc_generator.py
+++ neumann_bc_generator.py
@@ -10,6 +10,7 @@ def neumann_bc(rb):
   c2 = μp/(kB*temp)
   c3 = μe/(kB*temp)
   B_here = B0+Bd*rb
-  d_rho_2 = c2/(np.cosh(c2*B_here))**2
-  d_rho_3 = c3/(np.cosh(c3*B_here))**2
+  d_rho_2 = Bd*c2/(np.cosh(c2*B_here))**2
+  d_rho_3 = Bd*c3/(np.cosh(c3*B_here))**2
+  print("%e %e %e" % (d_rho_1,d_rho_2,d_rho_3))
   return [d_rho_1,d_rho_2,d_rho_3]
