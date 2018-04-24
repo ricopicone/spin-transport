4c43ef49f07cd13daea44d2b7257c29490f6c1dc
Cameron Devine <camerondtrain@gmail.com>
2018-04-24 14:04:17 -0700

    trying to get the BC generator working for Dirichlet BCs

--- bc_generator.py
+++ bc_generator.py
@@ -1,4 +1,4 @@
-
+import sys
 import numpy as np
 from physical_constants_etc import *
 from experimental_constants import *
@@ -21,15 +21,23 @@ def dirichlet_bc(rb):
   B_here = B0+Bd*rb
   # Langevin constants
   Ω_10 = (1+γb*δb)*(μp*Bd)/(kB*temp)
-  Ω_20 = -μp*Bb/(kB*temp)
-  Ω_30 = -μe*Bb/(kB*temp)
+  Ω_20 = -μp*B0/(kB*temp)
+  Ω_30 = -δb*γb*μe*B0/(kB*temp)
   # corresponding rhos
   rho_1 = -np.tanh(Ω_10/(1+δb))
-  rho_2 = -np.tanh(Ω_20 - 1/(1+γb*δb) * B_here/Bd * Ω_10)
-  rho_3 = -np.tanh(Ω_30 - γb*δb/(1+γb*δb) * B_here/Bd * Ω_10)
+  rho_2 = -np.tanh(Ω_20 - 1/(1+γb*δb) * (B_here - B0)/Bd * Ω_10)
+  rho_3 = -np.tanh(Ω_30 - γb*δb/(1+γb*δb) * (B_here - B0)/Bd * Ω_10)
   #print("%e %e %e" % (d_rho_1,d_rho_2,d_rho_3))
   return [rho_1,rho_2,rho_3]
 
 if __name__ == '__main__':
   rb = float(input('Enter r bar: '))
-  print(neumann_bc(rb))
+  try:
+    if sys.argv[1].lower() == 'neumann':
+      print(neumann_bc(rb))
+    if sys.argv[1].lower() == 'dirichlet':
+      print(dirichlet_bc(rb))
+    else:
+      print('{} is not an allowed boundary condition type, Use Dirichlet or Neumann'.format(sys.argv[1]))
+  except IndexError:
+    print('You must specicify either Neumann or Dirichlet BC as the first command line argument')
